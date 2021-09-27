58bf8abef2fa59d9608be1bd86401643b9984052
Trần Hữu Thanh <88789960+IT0505@users.noreply.github.com>
2021-09-27 14:59:01 +0700

    Update html, css, js
    Update

--- cntt/index.js
+++ cntt/index.js
@@ -2,10 +2,35 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     //document.getElementById("main").style.marginLeft = "250px";
     document.body.style.opacity = "0.6";
-  }
-  
-  function closeNav() {
+}
+
+function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     //document.getElementById("main").style.marginLeft= "0";
     document.body.style.opacity = "1";
-  }
+}
+function closeLogin() {
+    document.getElementById("loginForm").style.top = "-1200px";
+    document.getElementById("loginForm").style.zIndex = "0";
+    closeNav();
+}
+
+function closeRegister() {
+    document.getElementById("registerForm").style.top = "-700px";
+    document.getElementById("registerForm").style.zIndex = "0";
+    closeNav();
+}
+
+function openLogin() {
+    document.getElementById("loginForm").style.top = "-470px";
+    document.getElementById("loginForm").style.zIndex = "1";
+    closeRegister();
+    closeNav();
+}
+
+function openRegister() {
+    document.getElementById("registerForm").style.top = "0";
+    document.getElementById("registerForm").style.zIndex = "1";
+    closeLogin();
+    closeNav();
+}
